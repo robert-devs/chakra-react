b23d5444ee63b7167c3df9ab255ebe6a6f9f18d0
robert-devs <mainrobert04@gmail.com>
2023-02-20 10:52:40 +0300

    adding the responsiveness to sidebar and main

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,21 +1,27 @@
-import { Grid } from '@chakra-ui/react'
-import { GridItem } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 
 export default function RootLayout() {
   return (
-    <Grid templateColumns="repeat(6,1fr)" bg="gray.50">
+    <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
       <GridItem
         as="aside"
-        colSpan="1"
+        // colSpan="1"
+        colSpan={{ base: 6, lg: 2, xl: 1 }}
         bg="purple.400"
-        minHeight="100%"
-        p="30px"
+        minHeight={{ lg: '100vh' }}
+        // p="30px"
+        p={{ base: '20px', lg: '30px' }}
       >
         <span>Sidebar</span>
       </GridItem>
-      <GridItem as="main" colSpan="5" p="40px">
+      <GridItem
+        as="main"
+        // colSpan="5"
+        colSpan={{ base: 6, lg: 4, xl: 5 }}
+        p="40px"
+      >
         <Navbar />
         <Outlet />
       </GridItem>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,13 @@
-import { SimpleGrid, Box } from '@chakra-ui/react'
+import { SimpleGrid, Box, Text } from '@chakra-ui/react'
 
 export default function Dashboard() {
   return (
     <SimpleGrid columns={4} spacing="10px" minChildWidth="250px" p="10px">
-      <Box bg="white" h="200px" border="1px solid "></Box>
+      <Box bg="white" h="200px" border="1px solid ">
+        <Text color={{ base: 'pink.700', md: 'blue.700', lg: 'black.700' }}>
+          Hello
+        </Text>
+      </Box>
       <Box bg="white" h="200px" border="1px solid "></Box>
       <Box bg="white" h="200px" border="1px solid "></Box>
       <Box bg="white" h="200px" border="1px solid "></Box>
